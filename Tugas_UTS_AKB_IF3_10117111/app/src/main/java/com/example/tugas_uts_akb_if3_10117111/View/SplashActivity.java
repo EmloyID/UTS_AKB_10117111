3ce0c01e9e31db2d28ba65ea754ccba864d06024
Shendi Permana <43495545+ADoyenA@users.noreply.github.com>
2020-05-12 21:13:15 +0700

    Tias
    UTS AKB

---
+++ Tugas_UTS_AKB_IF3_10117111/app/src/main/java/com/example/tugas_uts_akb_if3_10117111/View/SplashActivity.java
@@ -0,0 +1,42 @@
+package com.example.tugas_uts_akb_if3_10117111.View;
+
+import android.content.Intent;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.example.tugas_uts_akb_if3_10117111.R;
+import com.viksaa.sssplash.lib.activity.AwesomeSplash;
+import com.viksaa.sssplash.lib.cnst.Flags;
+import com.viksaa.sssplash.lib.model.ConfigSplash;
+
+/** NIM : 10117111
+ * Nama : Tias Gumelar Timorisky
+ * Kelas : IF 3
+ * Tanggal : 12-05-2020**/
+
+public class SplashActivity extends AwesomeSplash {
+    @Override
+    public void initSplash(ConfigSplash configSplash){
+
+        //menambahkan background
+        configSplash.setBackgroundColor(R.color.colorAccent);
+        configSplash.setAnimCircularRevealDuration(3000);
+        configSplash.setRevealFlagX(Flags.REVEAL_LEFT);
+        configSplash.setRevealFlagX(Flags.REVEAL_BOTTOM);
+
+        //menambahkan logo
+        configSplash.setLogoSplash(R.drawable.logo);
+        configSplash.setAnimLogoSplashDuration(2000);
+        configSplash.setAnimTitleTechnique(Techniques.FadeIn);
+
+        //menambahkan title
+        configSplash.setTitleSplash("Make a Lots Of Friends");
+        configSplash.setTitleTextColor(R.color.colorPrimaryDark);
+        configSplash.setTitleTextSize(30f);
+        configSplash.setAnimTitleDuration(2000);
+        configSplash.setAnimTitleTechnique(Techniques.ZoomIn);
+    }
+    @Override
+    public void animationsFinished(){
+        startActivity(new Intent(SplashActivity.this, OnBoardView.class));
+    }
+}
